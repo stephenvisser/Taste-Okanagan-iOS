32e29d6657828aa2e199a1c472dda03cd325ab11
Jo-el van Bergen <johyphenel@gmail.com>
2012-03-03 11:47:23 -0800

    Initial app commit

---
+++ Taste Okanagan/FirstViewController.h
@@ -0,0 +1,13 @@
+//
+//  FirstViewController.h
+//  Taste Okanagan
+//
+//  Created by Jo-el van Bergen on 12-03-03.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FirstViewController : UIViewController
+
+@end
---
+++ Taste Okanagan/SecondViewController.h
@@ -0,0 +1,13 @@
+//
+//  SecondViewController.h
+//  Taste Okanagan
+//
+//  Created by Jo-el van Bergen on 12-03-03.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SecondViewController : UIViewController
+
+@end
